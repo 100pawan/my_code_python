36ef942d4f9348de8592622033e20fe611a16f46
100pawan <pt7697479@gmail.com>
2021-04-26 00:23:54 +0530

    string method

---
+++ string_method.py
@@ -0,0 +1,15 @@
+# string method 
+name = "pawan tiwari"
+
+# len() funtction
+print(len(name))
+# lower() method 
+print(name.lower())
+# upper() method 
+print(name.upper())
+
+# cout() method 
+print(name.count("a"))
+# title() method
+
+print(name.title())
