aa5c99a8825c9663a989f56adec4985547c3f094
100pawan <pt7697479@gmail.com>
2021-04-25 23:16:32 +0530

    string slicing

---
+++ string_slicing.py
@@ -0,0 +1,16 @@
+# string_slicing
+# [start arguments : stop arguments]
+
+lang = "python"
+
+# print(lang[0:3])
+
+# print(lang[0:])
+
+# print(lang[:6])
+
+print(lang[3:-1]) #slicing in backword
+
+
+
+ 
