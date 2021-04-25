ad8da0f750b31eff434abb6139ba18a15e2fe1ab
100pawan <pt7697479@gmail.com>
2021-04-25 23:30:23 +0530

    string slicing step

---
+++ string_slicing_step.py
@@ -0,0 +1,14 @@
+# string_slicing with step
+
+
+# syntax [start argumnets : stoparguments : step]
+
+name = "pawan"
+
+print(name[0:4:1])
+
+print(name[0::2])
+
+print(name[::-1])
+
+print(name[-1:-4:-1])
