d39603d881b2d679ee98d5376e55cb607bc2defd
100pawan <pt7697479@gmail.com>
2021-04-26 01:09:06 +0530

    striping remove_space problem

---
+++ stripmethod.py
@@ -0,0 +1,10 @@
+name = "     paw    an    "
+dots = "....................."
+
+print(name.lstrip()+ dots) #remove space from left side 
+
+print(name.rstrip()+ dots) # remove space from right side 
+
+print(name.strip()+ dots)
+
+print(name.replace(" ","") +dots)
