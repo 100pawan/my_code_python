dee0a1540319b05431a6842f44f86200fa1d432e
100pawan <pt7697479@gmail.com>
2021-04-27 23:14:30 +0530

    if statement

---
+++ if_statement.py
@@ -0,0 +1,14 @@
+# this is if statetment practice program
+
+
+name = input("Enter your name : ")
+
+print(name)
+
+age  = int(input("enter your age : "))
+
+if age >= 18:
+    print("shadi kr le abbb jake aaram se ")
+
+else:
+    print("abhi padh aram se ")
