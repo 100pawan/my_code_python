2c9ea0357781b4f371d4899155e5217566cbb34a
100pawan <pt7697479@gmail.com>
2021-04-27 18:24:34 +0530

    imutalbe string

---
+++ imutable_string.py
@@ -0,0 +1,11 @@
+# in python stirng are imutable its mean never modifed your original string its create the copy and print
+
+
+name = "pawan"
+
+# string[2] = "W" #its show error we can only use replace method but it not replce your original string
+# print(string)
+
+
+replace = name.replace("a", "A")
+print(replace)
