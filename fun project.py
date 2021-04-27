dd423d78d52528b45c42b5dcc1bdab85a8b652f5
100pawan <pt7697479@gmail.com>
2021-04-27 17:49:04 +0530

    find replace

--- fun project.py
+++
@@ -1,6 +0,0 @@
-import pyautogui, time
-time.sleep(5)
-
-while True:
-    pyautogui.typewrite("hey baby \U0001F61d") # code is for emoji 
-    pyautogui.press("enter")
---
+++ replace_string.py
@@ -0,0 +1,23 @@
+
+
+
+pawan = "pawan is a good boy is never cheat any one"
+
+# print(pawan.replace(" ", "_",))
+
+print(pawan.replace("is", "was", 1))
+
+print(pawan.replace("never", "alway",1))
+
+# finging the string postion
+
+find = pawan.find("is")
+
+print(find)
+
+
+# finding the position using conunt 
+
+find2 = pawan.find("is", find +1)
+
+print(find2)
