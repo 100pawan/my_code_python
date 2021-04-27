c37bf2def4daa7b9789dc4e18a89619b6b2dab51
100pawan <pt7697479@gmail.com>
2021-04-28 00:06:14 +0530

    and or

---
+++ and_or.py
@@ -0,0 +1,19 @@
+# this is and or condition program
+
+name = "pawan"
+
+age = 18
+
+# if name == "pawan" and age == 18:
+#     print("true")
+
+# else:
+#     print("false")    
+
+
+
+if name == "pawan" or age == 20:
+    print("true")
+
+else:
+    print("false")                 
