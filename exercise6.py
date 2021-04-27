c4ceb304098c64b122a8bac6a6a75a5fd2a52e28
100pawan <pt7697479@gmail.com>
2021-04-28 00:27:35 +0530

    exercise6

---
+++ exercise6.py
@@ -0,0 +1,9 @@
+# watch avenger movie program
+
+user_name = input("Enter your name :")
+user_age = (input("Enter your age :"))
+user_age = int(user_age)
+if user_age >= 18 and user_age==20 and (user_name[0]=='p' or user_name[0]=='P'):
+    print("you can watch your movie")
+else:
+    print("beta jakke padho abhi aram se ")    
