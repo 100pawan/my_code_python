b8f93703d2cbe2ef958e9a2dfe7694719b49b367
100pawan <pt7697479@gmail.com>
2021-04-29 01:17:44 +0530

    in key

---
+++ in keyword_if.py
@@ -0,0 +1,9 @@
+
+# in keyword with if statement
+# name = "pawan tiwari "
+name = input("enter your name :")
+
+if "a" in name:
+    print("a is present in your name ")
+else:
+    print("not")    
