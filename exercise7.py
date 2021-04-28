feb1b64eec119b9439a0ba1228dca551a1746cdf
100pawan <pt7697479@gmail.com>
2021-04-29 02:10:20 +0530

    exe

---
+++ exercise7.py
@@ -0,0 +1,15 @@
+# print natural number 1 ......n
+# user input
+# sum of input number to their range 
+
+
+total  = 0
+
+i = int(input("enter the number :"))
+
+n = 1
+
+while n<=i:
+    total += n
+    n +=1
+    print(total)    
