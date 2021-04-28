aad0918653f1a27853a4c93decb7d94669d1474c
100pawan <pt7697479@gmail.com>
2021-04-29 01:56:03 +0530

    while loop sum

---
+++ sum using loop.py
@@ -0,0 +1,24 @@
+# sum of the number using while loop 1 to 10
+
+# total = 0
+
+# i = 1
+
+# while i <= 10:
+#     total += i
+#     i += 1
+#     print(total)
+
+
+while True:
+    user = int(input("enter the number :"))
+
+    user1 = int(input("enter the number :"))
+
+    total = user + user1
+    print("Total sum of the number is :",str(total))
+
+# i = 1
+# while i <= i :
+#     print (total)
+#     i += 1
