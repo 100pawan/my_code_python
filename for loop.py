7f34e96edfebbf0dc48b45374cab8ac5227712b9
100pawan <pt7697479@gmail.com>
2021-04-30 15:51:44 +0530

    for loop

---
+++ for loop.py
@@ -0,0 +1,29 @@
+
+
+# this is for loop program
+
+# for i in range(10):
+#     print("hello word :")
+
+
+# sum of the number using for loop
+
+# total = 0
+
+# for i in range(1, 21):
+#     total += i 
+# print(total)    
+
+
+# sum of number by taking input from user
+
+
+n = int(input("enter the number :"))
+
+total = 0
+
+for i in range(1 , n+1):
+
+    total += i 
+
+print(total)    
